9ddbbbc8f94177310b1475f4b162eed8eca35442
BDeMo <382283735@qq.com>
2018-03-25 23:27:36 +0800

    update testing with lambda using training of thread pool.

--- src/main/java/Mytraining/Thread/DeadLockTest/DeadLockWithoutThreadPool.java
+++ src/main/java/Mytraining/Thread/DeadLockTest/DeadLockWithoutThreadPool.java
@@ -9,8 +9,8 @@ public class DeadLockWithoutThreadPool {
 	public static void main(String[] args) {
 		Object o1 = new Object();
         Object o2 = new Object();
-        new Thread(new MultiThread(o1, o2, 0), "flag = 0").start();;
-        new Thread(new MultiThread(o1, o2, 1), "flag = 1").start();;
+        new Thread(new MultiThread(o1, o2, 0), "flag = 0").start();
+        new Thread(new MultiThread(o1, o2, 1), "flag = 1").start();
 	}
 
 }
