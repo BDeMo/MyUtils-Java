d4a9072c3b766bb72b212f9c6462e099fe1fdc77
BDeMo <382283735@qq.com>
2018-03-25 20:18:43 +0800

    update testing with lambda using training of swing.

--- src/main/java/Mytraining/Lambda/Demo/InterationTesting.java
+++ src/main/java/Mytraining/Lambda/Demo/InterationTesting.java
@@ -1,5 +1,6 @@
 package Mytraining.Lambda.Demo;
 
+import java.net.SocketPermission;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,9 +16,12 @@ public class InterationTesting {
 		List<String> lst2 = Arrays.asList("Lambda", "Interation", "Testing", "List");
 		
 		lst1.forEach(System.out::println);
-		lst1.forEach(n -> System.out.println(n.getClass() + "" + n));
+		lst1.forEach(n -> System.out.println(n.getClass() + " " + n));
 		lst2.forEach(System.out::println);
 		lst2.forEach(n -> System.out.println(n.getClass() + n));
+
+		List intlst = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+		intlst.forEach(System.out::println);
 		
 	}
 
